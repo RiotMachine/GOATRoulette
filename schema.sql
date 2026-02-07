f6d034e89774b14c5c4aa30d2434d6536cda1e03
Jacob J <jirak87@outlook.com>
2026-02-06 18:56:27 -0600

    Adding triggers to enforce season length && playoff stage status.

--- schema.sql
+++ schema.sql
@@ -9,64 +9,103 @@
 PRAGMA foreign_keys = true;
 
 
-CREATE TABLE IF NOT EXISTS franchise (
+CREATE TABLE franchise (
     id INTEGER PRIMARY KEY,
 ) STRICT;
 
 
-CREATE TABLE IF NOT EXISTS team (
+CREATE TABLE team (
     franchise_id INTEGER NOT NULL REFERENCES franchise,
-    startDate INTEGER NOT NULL,
-    endDate INTEGER,
-    name TEXT NOT NULL,
+    startDate    INTEGER NOT NULL,
+    endDate      INTEGER,
+    name         TEXT NOT NULL,
   PRIMARY KEY (franchise_id, startDate),
 ) STRICT, WITHOUT ROWID;
 
-CREATE INDEX IF NOT EXISTS idx_franchise_id ON team(franchise_id);
+CREATE INDEX idx_franchise_id
+  ON team(franchise_id);
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_endDate_null
+CREATE UNIQUE INDEX idx_endDate_null
   ON team(franchise_id) WHERE endDate IS NULL;
 
 
-CREATE TABLE IF NOT EXISTS season (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE season (
+    id                   INTEGER PRIMARY KEY,
     regularSeason_length INTEGER NOT NULL
         CHECK (regularSeason_length >= 0),
-    length INTEGER NOT NULL
+    length               INTEGER NOT NULL
         CHECK (length >= regularSeason_length),
 ) STRICT;
 
 
-CREATE TABLE IF NOT EXISTS stage (
-    season_id INTEGER NOT NULL REFERENCES season,
-    stageNumber -- autoincrement according to season.length
-    isPlayoff -- bool autoset by increment > regularSeason_length
+CREATE TABLE stage (
+    season_id   INTEGER NOT NULL REFERENCES season,
+    stageNumber INTEGER NOT NULL,
+    isPlayoff   INTEGER NOT NULL DEFAULT FALSE
+        CHECK (isPlayoff IN (TRUE, FALSE)),
   PRIMARY KEY (season_id, stageNumber),
 ) STRICT, WITHOUT ROWID;
 
-CREATE INDEX IF NOT EXISTS idx_season_id ON stage(season_id);
+CREATE INDEX idx_season_id
+  ON stage(season_id);
+
+CREATE TRIGGER tgr_new_stageNumber
+  BEFORE INSERT ON stage
+    WHEN NEW.stageNumber > (SELECT length FROM season WHERE id = NEW.season_id)
+    BEGIN
+      SELECT RAISE (ROLLBACK, 'Stage number is greater than season.length');
+    END;
+
+CREATE TRIGGER tgr_modify_stageNumber
+  BEFORE UPDATE OF stageNumber ON stage
+    WHEN NEW.stageNumber > (SELECT length FROM season WHERE id = NEW.season_id)
+    BEGIN
+      SELECT RAISE (ROLLBACK, 'Stage number is greater than season.length');
+    END;
+
+CREATE TRIGGER tgr_newStage_setPlayoffBool
+  AFTER INSERT ON stage
+    WHEN NEW.stageNumber >
+      (SELECT regularSeason_length FROM season WHERE id = NEW.season_id)
+    BEGIN
+      UPDATE stage SET isPlayoff = TRUE WHERE season_id = NEW.season_id
+        AND stageNumber = NEW.stageNumber;
+    END;
+
+CREATE TRIGGER tgr_modifyStage_setPlayoffBool
+  AFTER UPDATE ON STAGE
+    WHEN NEW.stageNumber != OLD.stageNumber
+    BEGIN
+      UPDATE stage SET isPlayoff = IFF(stageNumber >
+          (SELECT regularSeason_length FROM season WHERE id = NEW.season_id),
+        TRUE, FALSE)
+      WHERE season_id = NEW.season_id AND stageNumber = NEW.stageNumber;
+    END;
 
 
-CREATE TABLE IF NOT EXISTS game (
-    id INTEGER PRIMARY KEY,
-    home_id INTEGER NOT NULL REFERENCES franchise,
-    away_id INTEGER NOT NULL REFERENCES franchise,
+CREATE TABLE game (
+    id            INTEGER PRIMARY KEY,
+    home_id       INTEGER NOT NULL REFERENCES franchise,
+    away_id       INTEGER NOT NULL REFERENCES franchise,
     atNeutralSite INTEGER NOT NULL DEFAULT FALSE
         CHECK (atNeutralSite IN (TRUE, FALSE)),
-    homeScore INTEGER NOT NULL
+    homeScore     INTEGER NOT NULL
         CHECK (homeScore >=0),
-    awayScore INTEGER NOT NULL
+    awayScore     INTEGER NOT NULL
         CHECK (awayScore >=0),
-    stageNumber INTEGER NOT NULL,
-    season_id INTEGER NOT NULL,
+    stageNumber   INTEGER NOT NULL,
+    season_id     INTEGER NOT NULL,
   FOREIGN KEY (stageNumber, season_id) REFERENCES stage (stageNumber, season_id),
 ) STRICT;
 
-CREATE INDEX IF NOT EXISTS idx_home_id ON game(home_id);
-CREATE INDEX IF NOT EXISTS idx_away_id ON game(away_id);
-CREATE INDEX IF NOT EXISTS idx_stageNumber_and_season_id ON game (stageNumber, season_id);
+CREATE INDEX idx_home_id
+  ON game(home_id);
+CREATE INDEX idx_away_id
+  ON game(away_id);
+CREATE INDEX idx_stageNumber_and_season_id
+  ON game (stageNumber, season_id);
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_home_id_and_away_id
+CREATE UNIQUE INDEX idx_home_id_and_away_id
   ON game(home_id, away_id);
 -- prevent new games for inactive franchises
 -- prevent new teams for inactive franchises
