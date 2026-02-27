54a8faf6563723f23c73528b11a75cd95cf7b3c2
Jacob J <jirak87@outlook.com>
2026-02-27 07:56:54 -0600

    Putting views in their own file.

---
+++ views.sql
@@ -0,0 +1,11 @@
+-- Season(desc) | Stage(asc) | isPlayoff | Game
+CREATE VIEW view_databaseFlow (
+    season_id,
+    stage_number,
+    isPlayoffStage,
+    game_id
+) AS SELECT season.id, stage.stageNumber, stage.isPlayoff, game.id
+    FROM season
+      INNER JOIN stage ON season.id = stage.season_id
+      INNER JOIN game  ON season.id = game.season_id AND stage.stageNumber = gam>
+    ORDER BY season.id DESC, stage.stageNumber ASC;
