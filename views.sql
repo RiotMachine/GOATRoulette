790728749bf8a05dbb21f4c5ee4d8060776ec682
Jacob J <jirak87@outlook.com>
2026-03-06 08:00:38 -0600

    Game View done.

--- views.sql
+++ views.sql
@@ -23,20 +23,27 @@ CREATE VIEW view_game (
     awayScore,
     homeTeam,
     homeScore
-) AS SELECT game.id, DATE(game.startDate), IF(atNeutralSite, 'Neutral site', homeTeam.city),
-    game.stageNumber, CONCAT(awayTeam.city, ' ', awayTeam.mascot), game.awayScore,
-    CONCAT(homeTeam.city, ' ', homeTeam.mascot), game.homeScore
+) AS SELECT 
+      game.id, 
+      DATE(game.startDate), 
+      IF(atNeutralSite, 'Neutral site', homeTeam.city),
+      game.stageNumber, 
+      CONCAT(awayTeam.city, ' ', awayTeam.mascot), 
+      game.awayScore,
+      CONCAT(homeTeam.city, ' ', homeTeam.mascot), 
+      game.homeScore
     FROM game
       INNER JOIN team AS awayTeam
-        ON game.away_id = team.franchise_id
+        ON game.away_id = awayTeam.franchise_id 
       INNER JOIN team AS homeTeam
-        ON game.home_id = team.franchise_id
+        ON game.home_id = homeTeam.franchise_id
 
 
 -- Season | Stage | Game in stage | Game details | Stage results to that point
 CREATE VIEW view_playoffs (
-    season,
+    season_id,
     round,
+    game,
     awayTeam,
     awayScore,
     homeTeam,
@@ -45,6 +52,21 @@ CREATE VIEW view_playoffs (
     awayTeam_wins,
     roundWinner
 ) AS SELECT 
+      game.season_id, 
+      game.stageNumber - season.regularSeason_length,
+
+      CONCAT(awayTeam.city, ' ', awayTeam.mascot),
+      game.awayScore,
+      CONCAT(homeTeam.city, ' ', homeTeam.mascot),
+      game.homeScore,
+
+  FROM game
+    INNER JOIN season
+      ON game.season_id = season.id
+    INNER JOIN team AS awayTeam
+      ON game.away_id = team.franchise_id
+    INNER JOIN team AS homeTeam
+      ON game.home_id = team.franchise_id
 
 
 -- Team | Season start | Season end | Regular szn wins | Reg szn losses | Playoff performance
