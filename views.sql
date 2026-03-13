a74407dcbba48d8b38b5dfe8baa8932f3d2c7042
Jacob J <jirak87@outlook.com>
2026-03-13 16:12:49 -0500

    Reforming views.

--- views.sql
+++ views.sql
@@ -9,81 +9,77 @@ CREATE VIEW view_databaseFlow (
       INNER JOIN stage
         ON season.id = stage.season_id
       INNER JOIN game
-        ON season.id = game.season_id AND stage.stageNumber = game.stageNumber;
+        ON season.id = game.season_id 
+          AND stage.stageNumber = game.stageNumber;
 
 
--- Game_id | Time | Location | Stage |
--- Away | Away score | Home | Home score
 CREATE VIEW view_game (
     id,
+    stage,
+    season_id,
     time,
     location,
-    stage,
+    away_id
     awayTeam,
     awayScore,
+    home_id
     homeTeam,
     homeScore
 ) AS SELECT 
-      game.id, 
-      DATE(game.startDate, 'unixepoch'), 
-      IF(atNeutralSite, 'Neutral site', homeTeam.city),
-      game.stageNumber, 
-      CONCAT(awayTeam.city, ' ', awayTeam.mascot), 
-      game.awayScore,
-      CONCAT(homeTeam.city, ' ', homeTeam.mascot), 
-      game.homeScore
-    FROM game
-      INNER JOIN team AS awayTeam
-        ON game.away_id = awayTeam.franchise_id
-          AND game.startDate >= awayTeam.startDate
-          AND (game.startDate <= awayTeam.endDate OR awayTeam.endDate IS NULL) 
-      INNER JOIN team AS homeTeam
-        ON game.home_id = homeTeam.franchise_id
-          AND game.startDate >= homeTeam.startDate
-          AND (game.startDate <= homeTeam.endDate OR homeTeam.endDate IS NULL);
+       game.id, 
+       game.stageNumber,
+       game.season_id, 
+       DATE(game.startDate, 'unixepoch'), 
+       IF(atNeutralSite, 'Neutral site', homeTeam.city),
+       awayTeam.franchise_id,
+       CONCAT(awayTeam.city, ' ', awayTeam.mascot), 
+       game.awayScore,
+       homeTeam.franchise_id,
+       CONCAT(homeTeam.city, ' ', homeTeam.mascot), 
+       game.homeScore
+     FROM game
+       INNER JOIN team AS awayTeam
+         ON game.away_id = awayTeam.franchise_id
+           AND game.startDate >= awayTeam.startDate
+           AND (game.startDate <= awayTeam.endDate OR awayTeam.endDate IS NULL) 
+       INNER JOIN team AS homeTeam
+         ON game.home_id = homeTeam.franchise_id
+           AND game.startDate >= homeTeam.startDate
+           AND (game.startDate <= homeTeam.endDate OR homeTeam.endDate IS NULL);
 
 
--- Season | Stage | Game in stage |
--- Game details | Stage results to that point
 CREATE VIEW view_playoffs (
-    season_id, round, game,
-    team_1, 
-    team_1_score,
-    team_2, 
-    team_2_score,
-    team_1_wins, 
-    team_2_wins
-) AS
-    WITH gameDeets (
-      season_id, round,
-      team_1_id, team_1_score,
-      team_2_id, team_2_score,
-      startDate
-    )
-    AS (
-      SELECT 
-        game.season_id,
-        game.stageNumber + 1 - MIN(game.stageNumber) OVER (
-          PARTITION BY game.season_id
-        ),
-        MIN(game.away_id, game.home_id),
-        IF (MIN(game.away_id, game.home_id) = game.home_id, game.homeScore, game.awayScore),
-        MAX(game.away_id, game.home_id),
-        IF (MAX(game.away_id, game.home_id) = game.home_id, game.homeScore, game.awayScore),
-        game.startDate
-      FROM game
-        INNER JOIN stage
-          ON game.season_id = stage.season_id
-            AND game.stageNumber = stage.stageNumber
-            AND stage.isPlayoff = TRUE
-    )            
-    SELECT
-      season_id, round,
-      ROW_NUMBER() OVER (
-        window_series
-      ),
-      CONCAT(team_1.city, ' ', team_1.mascot), team_1_score,
-      CONCAT(team_2.city, ' ', team_2.mascot), team_2_score,
+    game_id,
+    round,
+    round_game,
+    season_id,
+    time,
+    location,
+    team1,
+    team1_score
+    team2,
+    team2_score
+) AS 
+     SELECT 
+       v_game.id,
+       v_game.stageNumber + 1 - MIN(v_game.stageNumber) OVER (
+         PARTITION BY v_game.season_id
+       ),
+       ROW_NUMBER() OVER (
+         window_series
+       ),
+       v_game.season_id,
+       v_game.time,
+       v_game.location,
+
+
+     FROM stage
+     WHERE isPlayoff = TRUE
+       INNER JOIN view_game AS v_game
+         ON v_game.stage = stage.stageNumber
+           AND v_game.season_id = stage.season_id            
+
+
       SUM(IF(team_1_score > team_2_score, 1, 0)) OVER (
         window_series
         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
@@ -92,25 +88,15 @@ CREATE VIEW view_playoffs (
         window_series
         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
       )
-    FROM gameDeets
-      INNER JOIN team AS team_1
-        ON gameDeets.team_1_id = team_1.franchise_id
-          AND gameDeets.startDate >= team_1.startDate
-          AND (gameDeets.startDate <= team_1.endDate OR team_1.endDate IS NULL) 
-      INNER JOIN team AS team_2
-        ON gameDeets.team_2_id = team_2.franchise_id
-          AND gameDeets.startDate >= team_2.startDate
-          AND (gameDeets.startDate <= team_2.endDate OR team_2.endDate IS NULL) 
-    WINDOW window_series AS (
-      PARTITION BY
-        season_id, 
-        round,
-        team_1_id,
-        team_2_id
-      ORDER BY
-        gameDeets.startDate
-      );
-
+     WINDOW window_series AS (
+       PARTITION BY
+         season_id, 
+         round,
+         team_1_id,
+         team_2_id
+       ORDER BY
+         gameDeets.startDate
+     );
 
 
 -- Team | Season start | Season end | Regular szn wins | Reg szn losses | Playoff performance
