a1eda34a1fc259df157265f1c0525b8bdabc40e3
Jacob J <jirak87@outlook.com>
2026-05-23 20:32:05 -0500

    Wrote typesafe IDs.

--- Application/Element.h
+++ Application/Element.h
@@ -1,20 +1,48 @@
 #ifndef ELEMENT_H
 #define ELEMENT_H
 
-enum class Source
-{
-    database,
-    model,
-    user
-};
-
 struct Element
 {
-    Source source{ Source::database };
+    enum Source
+    {
+        database,
+        model,
+        user
+    };
+
+    // nice solution to make IDs typesafe
+    // https://www.ilikebigbits.com/2014_05_06_type_safe_handles.html
+    template <typename Tag>
+    struct ID
+    {
+    public:
+        // explicit prevents implicit conversion of ints
+        explicit ID(int x) : m_val{ x } {}
+        // allowing default constructor for sim objects
+        ID() = default;
+
+        bool isValid() const { return m_val != s_invalidVal; }
+
+        // force explicit cast to int
+        explicit operator int() const { return m_val; }
+
+        friend bool operator==(ID a, ID b) { return a.m_val == b.m_val; }
+        friend bool operator!=(ID a, ID b) { return a.m_val != b.m_val; }
+
+    private:
+        // default state is invalid
+        static constexpr int s_invalidVal{ -1 };
+        int m_val{ s_invalidVal };
+    };
+
+    Source source{ model };
 };
 
 struct Game : Element
 {
+    using ID = ID<struct GameTag>;
+
+    ID id{ };
 
 };
 
