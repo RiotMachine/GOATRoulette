d48d2cb8c226b0111052a63474b26e680bee9a25
Jacob J <jirak87@outlook.com>
2026-05-27 16:58:56 -0500

    Filling out Element structs.

--- Application/Element.h
+++ Application/Element.h
@@ -4,10 +4,13 @@
 #include "aliases.h"
 #include "wrappers.h"
 #include <cstdint>
+// for std::is_aggregate
+#include <type_traits>
 
 // Preventing Element instantiation would ruin inheritors' aggreg status
 struct Element
 {
+    // IDs map to SQL IDs or array indices at crossover points
     using IDType = std::int32_t;
 
     enum Source
@@ -20,26 +23,35 @@ struct Element
     Source source{ model };
 };
 
-// IDs map to SQL IDs or array indices at crossover points
 
+// Team maps to SQL.franchise
+// Team takes most-recent SQL.team info as printing data
+/// determined by most-recent startDate
 struct Team : Element
 {
+    static_assert(std::is_aggregate_v<Team>);
     using ID = Wrapper::ID<struct TeamTag, IDType>;
 
     ID id{ ID::next() };
-
+    std::string city{ };
+    std::string mascot{ };
+    Alias::UnixTime startDate{ };
 };
 
 struct Season : Element
 {
+    static_assert(std::is_aggregate_v<Season>);
     using ID = Wrapper::ID<struct SeasonTag, IDType>;
 
     ID id{ ID::next() };
-
+    int stages{ };
+    Alias::UnixTime startDate{ };
+    Alias::UnixTime endDate{ };
 };
 
 struct Stage : Element
 {
+    static_assert(std::is_aggregate_v<Stage>);
     struct IDPair
     {
         Season::ID seasonID{ };
@@ -47,11 +59,12 @@ struct Stage : Element
     };
 
     IDPair idPair{ };
-
+    bool isPlayoff{ };
 };
 
 struct Game : Element
 {
+    static_assert(std::is_aggregate_v<Game>);
     using ID = Wrapper::ID<struct GameTag, IDType>;
     struct Opponent
     {
