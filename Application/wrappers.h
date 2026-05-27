4424cabb2833a6fdbd5759c513044bfb30d97ac6
Jacob J <jirak87@outlook.com>
2026-05-27 16:52:27 -0500

    Added comparison operators and adjusted ID::sentinelVal naming

--- Application/wrappers.h
+++ Application/wrappers.h
@@ -7,7 +7,7 @@ namespace Wrapper
 {
     // typesafe IDs
     // https://www.ilikebigbits.com/2014_05_06_type_safe_handles.html
-    template <typename Tag, typename T=int, T sentinel=-1>
+    template <typename Tag, typename T=int, T sentinelVal=-1, T startVal=1>
     struct ID
     {
     public:
@@ -16,20 +16,22 @@ namespace Wrapper
         // allowing default constructor for sim objects
         ID() = default;
 
-        static ID invalid() { return ID{ sentinel }; }
-        static ID next()    { return ID{ s_nextVal++ }; }
+        static ID sentinel() { return ID{ sentinelVal }; }
+        static ID next()     { return ID{ s_nextVal++ }; }
 
         // force explicit cast to integral
         explicit operator T() const { return m_val; }
 
+        friend bool operator< (ID a, ID b) { return a.m_val <  b.m_val; }
+        friend bool operator> (ID a, ID b) { return a.m_val >  b.m_val; }
         friend bool operator==(ID a, ID b) { return a.m_val == b.m_val; }
         friend bool operator!=(ID a, ID b) { return a.m_val != b.m_val; }
 
     private:
         // default state is invalid
-        T m_val{ sentinel };
+        T m_val{ sentinelVal };
         // atomic prevents race conditions
-        static inline std::atomic<T> s_nextVal{ 1 };
+        static inline std::atomic<T> s_nextVal{ startVal };
     };
 
 }
