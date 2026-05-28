19c7d4676a63f65355b1498b2e655aa5cb5e4cc5
Jacob J <jirak87@outlook.com>
2026-05-28 18:09:52 -0500

    Building out Controller!

--- Application/Controller.h
+++ Application/Controller.h
@@ -1,16 +1,44 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include "Config.h"
+
+// Forward declarations
+struct Element;
+
+
 class Controller
 {
 public:
+    Controller(Config config)
+        : sqlMap{ config.dbPath },
+          lookback{ config.defaultLookback }
+    {
+        
+    }
+
+    ~Controller()
+    {
+    
+    }
 
 
+    Element pull( )
+    {
 
+    }
+
+    bool push(Element e)
+    {
+    
+    }
 
 
 private:
-
+    SQLMap sqlMap{ };
+    DataMap dataMap{ };
+    std::array<Repository, > repo{ };
+    std::chrono::days lookback{ };
 
 };
 
